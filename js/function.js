17afbdc6711988b705f09a5476c77a6a91125616
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-07 16:33:27 +0200

    fixed bug img

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 function createdImg(percorso, titolo, testo) {
     return `
-            <div class="card text-bg-dark my-3 w-50 m-auto">
+            <div class="card text-white py-3 w-50 m-auto my-2">
                 <img src="${percorso}" class="card-img" alt="...">
                 <div class="text">
                     <h5 class="">${titolo}</h5>
