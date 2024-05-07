636476a95b7cc6ab2764425d69b1e2d641480953
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-07 21:25:51 +0200

    m-2 added button inside html and if inside cycle in js

--- js/function.js
+++ js/function.js
@@ -1,7 +1,7 @@
-function createdImg(percorso, titolo, testo) {
+function createdImg(percorso, titolo, testo, classe) {
     return `
-            <div class="card text-white py-3 w-50 m-auto my-2">
-                <img src="${percorso}" class="card-img image block" alt="...">
+            <div class="card text-white py-3 w-50 m-auto my-2 ${classe}">
+                <img src="${percorso}" class="card-img image" alt="...">
                 <div class="text">
                     <h5 class="">${titolo}</h5>
                     <p class="">${testo}</p>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 //richiamo il tag div con classe container dall'html
 let div = document.querySelector("div")
 
-let arrayImg = document.getElementsByClassName("image")
-console.log(arrayImg)
+let iActive
 
 let imgBlock;
 let nextImg;
@@ -37,16 +36,60 @@ document.getElementById("next").addEventListener("click", function () {
     // Creo ciclo for che mi cicla sulla lunghezza della lista e dentro richiao una funzione che ogni giro mi crea l'immagine da mettere
     for (let i = 0; i < objectsImages.length; i++) {
         let arrayImages = objectsImages[i]
-        div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text)
+        if (iActive = i) {
+            console.log(i)
+            div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "none")
+        }
+        else {
+            console.log(i)
+            div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "block")
+        }
+        
+
+        // nextImg = imgBlock + 1                                         NON FUNZIONA
+        // if (nextImg >= objectsImages.length) {
+        //     nextImg = 0
+        // }
+
+        // //se img contiene block allora glielo rimuovo                 NON FUNZIONA
+        // if (iActive = 0) {
+        //     div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "none")
+        //     //do alla variabile imgBlock lo stesso valore di i senno non viene visto fuori dal ciclo questo valore
+        //     imgBlock = iActive;
+        // }
     }
-    //se img contiene block allora glielo rimuovo
-    if (arrayImages.classList.contains("block")) {
-        arrayImages.classList.remove("block");
-        console.log("ho tolto block all'immagine?", i);
-        //do alla variabile imgBlock lo stesso valore di i senno non viene visto fuori dal ciclo questo valore
-        imgBlock = i;
-    }
-    
+
 })
+
+//Richiamo tramite id il bottone dell'html
+document.getElementById("previous").addEventListener("click", function () {
+    //Inizio ciclo
+    for (let i = 0; i < objectsImages.length; i++) {
+        let arrayImages = objectsImages[i]
+        if (iActive = i) {
+            console.log(i)
+            div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "none")
+            
+        }
+        else {
+            console.log(i)
+            div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "block")
+            iActive = i - 1
+        }
+
+        // nextImg = imgBlock - 1                                      NON FUNZIONA
+        // if (nextImg < 0) {
+        //     nextImg = objectsImages.length - 1
+        // }
+
+        // //se img contiene block allora glielo rimuovo                NON FUNZIONA
+        // if (iActive = 0) {
+        //     div.innerHTML += createdImg(arrayImages.image, arrayImages.title, arrayImages.text, "none")
+        //     //do alla variabile imgBlock lo stesso valore di i senno non viene visto fuori dal ciclo questo valore
+        //     imgBlock = iActive;
+        // }
+    }
+})
+
 
 
